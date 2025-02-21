56794f71442f6f02bf2c032fa17ab0779d22faac
Rennie Benoit <benoit.ren01@outlook.fr>
2025-02-21 15:30:19 +0100

    update login.js

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -7,30 +7,30 @@ document.addEventListener("DOMContentLoaded", () => {
     form.addEventListener("submit", async (event) => {
       event.preventDefault();
   
-      const userData = {
+      const userData = { 
         email: emailInput.value,
         password: passwordInput.value,
       };
   
-      try {
+     try  { //******************utilistation de try */
         const response = await fetch("http://localhost:5678/api/users/login", { 
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify(userData),
+          body: JSON.stringify(userData),  //************************convertir en JSON avant l' envoie au serv */
         });
   
         const data = await response.json();
-        console.log("Réponse serveur :", data);
+        console.log("Réponse serveur complète :", data);//*******************************probleme de route */
         if (response.ok) {
-          localStorage.setItem("authToken", data.token); 
+          localStorage.setItem("authToken", data.token);  // 
           window.location.href = "index.html"; 
         } else {
           errorMessage.textContent = "Identifiants incorrects !";
           errorMessage.style.display = "block"; 
         }
-      } catch (error) {
+      } catch (error) { // ********************************************d' ou try 
         console.error("Erreur lors de la connexion :", error);
         errorMessage.textContent = "Problème de connexion au serveur.";
         errorMessage.style.display = "block";
