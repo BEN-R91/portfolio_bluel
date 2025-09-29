7f311e37d222462cea2793792b3006ee217daeec
Rennie Benoit <benoit.ren01@outlook.fr>
2025-09-29 06:01:20 +0200

    Generating the figure element

--- FrontEnd/assets/js/getWorks.js
+++ FrontEnd/assets/js/getWorks.js
@@ -17,21 +17,33 @@ console.log(works);
 const getWorks = async () => {
     
     try {
-        const response = await fetch('http://localhost:5678/api/works');
+        const response = await fetch('http://localhost:5678/api/works'); //pourquoi ' et plus "//
         const worksData = await response.json();
         console.log("Récupération des travaux terminée", worksData)
         return worksData;
     }
 
     catch (error) { /*revoir role du catch (error)*/
-        console.error("Erreur lors de la récupération des travaux", error); /*role du "error" apres la virgule*/
+        console.error("Erreur lors de la récupération des travaux", error); //role du "error" apres la virgule//
         return []; /*pourquoi retourner un tableau vide est essentiel*/
     }
 }
 
-const createFigure = () => {
-    // 2.
-}
+const createFigure = (work) => { /*WORK ??????*/
+    
+    const figureElement = document.createElement('figure'); //création de l' élément "figure" (conteneur parent)//
+    const imageElement = document.createElement('img'); //création de l' élément "img"//
+    imageElement.src = work.imageUrl; //Récupère l' URL (src)
+    imageElement.alt = work.title; //Récupère le tire (alt)
+
+    const captionElement = document.createElement('figcaption'); //création de l'élément "figcaption"
+    captionElement.innerText = work.title; //ajout du texte de la légende
+
+    figureElement.appendChild(imageElement); //ASSEMBLAGE : on place <img> et <figcaption> dans <figure>
+    figureElement.appendChild(captionElement);
+
+    return figureElement; //ON RETOURNE l' élément <figure> complet
+}                                                                              //Pour creatFigure REVISER FONCTIONNEMENT ET WORK + RAJOUT log pour verif fonctionnement si possible
 
 const insertInContainer = () => {
     // 3.
