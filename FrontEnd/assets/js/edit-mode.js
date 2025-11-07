c2e2f14838ae942c2fd23dcbe1218920b01cfbae
Rennie Benoit <benoit.ren01@outlook.fr>
2025-11-07 11:41:08 +0100

    add openModal.js

--- FrontEnd/assets/js/edit-mode.js
+++ FrontEnd/assets/js/edit-mode.js
@@ -4,10 +4,12 @@ export const isLogged = !!token
 
 const banner = document.querySelector(".edit-mode-banner");
 const loginLink = document.querySelector('nav ul li a[href="login-page.html"]');
+const editButton = document.querySelector(".edit-button");
 
 if (isLogged) {
   banner.style.display = "flex";
   loginLink.textContent = "logout";
+  editButton.style.display = "inline-block";
   
   loginLink.addEventListener("click", function (event) {
     event.preventDefault();
@@ -17,4 +19,5 @@ if (isLogged) {
 } else {
   banner.style.display = "none";
   loginLink.textContent = "login";
+  editButton.style.display = "none";
 }
