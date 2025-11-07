5129a1950b4f591aee70da084025cc10ae5cc82f
Rennie Benoit <benoit.ren01@outlook.fr>
2025-11-07 12:08:39 +0100

    modale.css

--- FrontEnd/assets/js/login-page.js
+++ FrontEnd/assets/js/login-page.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const response = await fetch(url_api, {
       method: "POST", 
       headers: { "Content-Type": "application/json" }, //HEADERS HTTP!! Content-Type: application/json → “je t’envoie du JSON” autres ex : Authorization: Bearer XXXXXX → “voici mon jeton d’accès” Authorization: Bearer XXXXXX → “voici mon jeton d’accès” Accept: application/json → “je veux une réponse au format JSON”
-      body: JSON.stringify({ email, password }) // on transforme les données en JSON (texte brut pour que le serv comprenne jemploie JSON.stringify) et donc données concernées email et pwd. body= corp/contenu de la requete
+      body: JSON.stringify({ email, password }) // on transforme les données en JSON (chaine de caractères pour que le serv comprenne jemploie JSON.stringify) et donc données concernées email et pwd. body= corp/contenu de la requete
     });
     
     if (response.ok) {
