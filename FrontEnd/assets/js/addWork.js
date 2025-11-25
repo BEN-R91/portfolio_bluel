7d819af7caa11968229e200585f0c79ae3e523f1
Rennie Benoit <benoit.ren01@outlook.fr>
2025-11-25 22:48:58 +0100

    css addModal ok

---
+++ FrontEnd/assets/js/addWork.js
@@ -0,0 +1,8 @@
+/**
+ * Remplir le formulaire et surtout sélectionner la photo.
+ * 
+ * 1. Lier le label et le input type file pour que le label devienne le bouton du input [OK]
+ * 2. L'utilisateur va sélectionner une photo, écouter la sélection avec un event de type "change" ou "input"
+ * 3. Récupérer la valeur dans le input
+ * 4. Remplacer toute la div bleue (cacher en CSS) par la photo récupéré au point 3.
+ */
